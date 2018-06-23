c61abec6b1cd17595cbbca530c9d05d285f9e111
Hugo Borges <hugobp14@hotmail.com>
2018-06-23 20:56:48 -0300

    Updated app body

--- inc/config.hpp
+++ inc/config.hpp
@@ -8,7 +8,7 @@
 #ifndef INC_CONFIG_HPP_
 #define INC_CONFIG_HPP_
 
-#define DEBUG 1
+#define CONFIG_DEBUG                         1
 
 #define CONFIG_PORT                          (uint16_t) 12345
 #define CONFIG_CONNECTION_BUFFER_SIZE        (uint32_t) 8000
--- src/app.cpp
+++ src/app.cpp
@@ -1,11 +1,29 @@
 #include <cstdint>
+#include <iostream>
 
 #include "config.hpp"
-#include "model/server.hpp"
+
+#include "model/socket/server.hpp"
+#include "model/socket/exceptions/exception-port-in-use.hpp"
+
+#include "model/loggers/console-logger.hpp"
+
+using namespace std;
 
 int main(int argc, char *argv[]){
 
-	SocketServer server(12345);
-	uint16_t port = CONFIG_PORT;
+	SocketServer server(CONFIG_PORT);
+	Logger *logger_app = new ConsoleLogger("APP", CONFIG_DEBUG);
+
+	try
+	{
+
+		server.start();
+	}
+	catch(ExceptionPortInUse &ex)
+	{
+		logger_app->error(ex.what());
+	}
+
 	return 0;
 }
